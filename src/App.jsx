cd1b17f9740ad3c7153e7f33c3f16dc3e4bb33e5
Faique Inaas MZM <faiqueinaas@gmail.com>
2025-03-01 23:12:03 +0530

    project setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
-import Card from "../src/components/card";
-
 const App = () => {
   return (
-    <div className="flex flex-col justify-center items-center">
-      <Card title="Star Wars" rating={5} isCool={true} />
-      <Card title="Avatar" />
-      <Card title="Lion King" />
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello World!</h1>
     </div>
   );
 };
