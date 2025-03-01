841ed289b0567efbf53ae4996d7a32ca6ff8181a
Faique Inaas MZM <faiqueinaas@gmail.com>
2025-03-01 22:25:00 +0530

    added useState

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import Card from "../src/components/card";
 
 const App = () => {
   return (
-    <div className="flex flex-col justify-center items-center w-5xl">
-      <Card title="Star Wars" />
+    <div className="flex flex-col justify-center items-center">
+      <Card title="Star Wars" rating={5} isCool={true} />
       <Card title="Avatar" />
       <Card title="Lion King" />
     </div>
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,7 +1,22 @@
+import { useState } from "react";
+
 const Card = ({ title }) => {
+  const [hasLiked, setHasLiked] = useState(false);
+
   return (
-    <div className="border border-gray-300 p-5 m-2 bg-gray-600">
+    <div className="border border-gray-300 p-5 m-2 bg-gray-600 flex flex-col items-start">
       <h1 className="text-4xl text-white">{title}</h1>
+      <div className="w-full flex justify-end mt-2">
+        <button
+          className={`px-2 border border-black rounded cursor-pointer ${
+            hasLiked ? "bg-blue-500" : "bg-transparent"
+          }`}
+          // onClick={() => setHasLiked((prev) => !prev)}
+          onClick={() => setHasLiked(!hasLiked)}
+        >
+          {hasLiked ? "Liked" : "Like"}
+        </button>
+      </div>
     </div>
   );
 };
